0d128727fe9c28a1cd424cb053676f135e8a0fed
ZhixiaoSu <zhixiaosu@gmail.com>
2020-10-28 15:01:04 -0400

    currComputer

---
+++ matlabLibraries/myFunctions/currComputer.m
@@ -0,0 +1,12 @@
+function [root, sep] = currComputer()
+
+if ismac
+    root = '/Volumes/cooper/';
+  %  root = '/Volumes/bbari1/';
+    sep = '/';
+elseif ispc
+  %  root = 'D:\';
+  %  root = 'Z:\';
+    root = 'C:\Users\zhixi\Documents\data\';
+    sep = '\';
+end
